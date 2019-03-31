b362c82a5396fecdfb215b809f6bbde6f0b6c581
Krzysztof <krzbeszczynski@gmail.com>
2019-03-31 11:05:16 +0200

    commit

--- app/src/main/java/com/example/studentwsb/myappfirebase/MainActivity.java
+++ app/src/main/java/com/example/studentwsb/myappfirebase/MainActivity.java
@@ -4,10 +4,14 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -77,6 +81,29 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         };
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String emailString = email.getText().toString();
+                String passwordString = password.getText().toString();
+
+                if(!emailString.equals("") && passwordString.equals("")){
+                    mAuth.signInWithEmailAndPassword(emailString,passwordString)
+                            .addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if(!task.isSuccessful()){
+                                        Toast.makeText(MainActivity.this, "Błędne dane", Toast.LENGTH_LONG).show();
+                                    }else {
+                                        Toast.makeText(MainActivity.this, "Zalogowano",Toast.LENGTH_LONG).show();
+                                    }
+                                }
+                            });
+
+                }
+            }
+        } );
 
 
 
